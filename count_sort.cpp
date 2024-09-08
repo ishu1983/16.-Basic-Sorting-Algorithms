02d60451d3b6561eef94c4e08a45be356ca56fc2
Ishu <ishubarnwal1225@gmail.com>
2024-09-08 21:01:40 +0530

    Initial commit

---
+++ count_sort.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+    void print(int arr[], int n){
+    for(int i = 0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void countSort(int arr[], int n){
+    int freq[10000];
+    int minVal = INT8_MAX, maxVal = INT8_MIN;
+    for(int i=0; i<n; i++){
+        minVal = min(minVal, arr[i]);
+        maxVal = max(maxVal, arr[i]);
+    }
+
+    for(int i=0; i<n; i++){
+        freq[arr[i]]++;
+    }
+
+    for(int i=minVal, j=0; i<=maxVal; i++){
+        while(freq[i]>0){
+            arr[j++] = i;
+            freq[i]--;
+        }
+    }
+    print(arr, n);
+}
+int main(){
+    int arr[8] = {1, 4, 1, 3, 2, 4, 3, 7};
+    countSort(arr, 8);
+    return 0;
+}
